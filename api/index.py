5606f43635bc9159f8dec89a1a8b2613e71cb987
Nika mgaloblishvili <nikolozamgalo@gmail.com>
2024-08-26 12:14:11 +0300

    Fix module crash

--- api/index.py
+++ api/index.py
@@ -1,7 +1,9 @@
 from flask import Flask, abort
 
-from main import fetch_csrf, auth_and_fetch_balance
 from flask import request
+from bs4 import BeautifulSoup
+import json
+import requests
 
 app = Flask(__name__)
 
@@ -26,5 +28,26 @@ def fetch_balance():
     except:
         abort(401)
 
+
+BASE_URL = "https://www.magticom.ge/"
+
+
+def fetch_csrf():
+    csrf_request = requests.get(BASE_URL + "mymagti")
+    request_cookies = csrf_request.cookies.get_dict()
+    soup = BeautifulSoup(csrf_request.text, "html.parser")
+    return soup.find("input", {"name": "csrf-token-login"}).get("value"), request_cookies
+
+
+def auth_and_fetch_balance(credentials, csrf_token, cookies):
+    data = {
+        "subscriber": credentials["username"],
+        "password": credentials["password"],
+        "csrf-token-login": csrf_token
+    }
+    requests.post(BASE_URL + "request/log-in.php", data=data, cookies=cookies)
+    portal = requests.get(BASE_URL + "request/accounts.php", cookies=cookies)
+    raw_data = json.loads(portal.text)
+    return raw_data["balances"][0]["balance"]
 
 app.run(debug=True, host='0.0.0.0', port=80)
